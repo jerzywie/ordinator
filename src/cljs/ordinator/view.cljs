5ed5c8d28a7af61e7f2450386ad75e4334378fb9
Jerzy Wieczorek <jerzywie@razzydo.co.uk>
2016-07-18 19:27:42 +0100

    Top level view changes for order view

--- src/cljs/ordinator/view.cljs
+++ src/cljs/ordinator/view.cljs
@@ -3,8 +3,9 @@
             [ordinator.utils :as utils]
             [cljs.ordinator.routes :refer [href-for]]
             [cljs.ordinator.messages :as m]
-            [cljs.ordinator.login.view :as login]
-            [cljs.ordinator.login.messages :as login-messages :refer [DoLogout]]))
+            [cljs.ordinator.login.messages :as login-messages :refer [DoLogout]]
+            [cljs.ordinator.login.view :as login-view]
+            [cljs.ordinator.order.view :as order-view]))
 
 (defn sign-out! [])
 
@@ -14,15 +15,16 @@
 (defn about-page []
   (fn [] [:div "about-page"]))
 
-(defn order-page []
-  (fn [] [:div "order-page"]))
+(defn order-page [ui-channel app]
+  [:div
+   [order-view/root (forward m/->Order ui-channel) (:order app)]])
 
 (defn allorders-page []
   (fn [] [:div "all-orders-page"]))
 
 (defn login-page [ui-channel app]
   [:div
-   [login/root (forward m/->Login ui-channel) (:login app)]])
+   [login-view/root (forward m/->Login ui-channel) (:login app)]])
 
 (defn header [ui-channel app]
   [:nav.navbar.navbar-default.navbar-fixed-top
@@ -34,15 +36,16 @@
       [:li [:a {:href (href-for :order-page)} "View your order"]]
       [:li [:a {:href (href-for :about-page)} "About"]]
       [:li [:a {:href (href-for :allorders-page)} "View collated order"]]
-      [:li [:a {:href (href-for :login-page)} "Login"]]]
-     [login/user-status-widget (forward m/->Login ui-channel) (:login app)]]]])
+      ;[:li [:a {:href (href-for :login-page)} "Login"]]
+      ]
+     [login-view/user-status-widget (forward m/->Login ui-channel) (:login app)]]]])
 
 (defn root [ui-channel app]
   [:div
    [header ui-channel app]
    (case (-> app :view :handler)
      :about-page [about-page]
-     :order-page [order-page]
+     :order-page [order-page ui-channel app]
      :allorders-page [allorders-page]
      :login-page [login-page ui-channel app]
      [home-page ui-channel app])
