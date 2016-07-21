17e84504a167d076010741bf90c75c39250a7422
Jerzy Wieczorek <jerzywie@razzydo.co.uk>
2016-07-21 22:28:52 +0100

    Improve organisation

--- src/cljs/ordinator/login/processing.cljs
+++ src/cljs/ordinator/login/processing.cljs
@@ -5,48 +5,45 @@
             [ordinator.utils :as utils]))
 
 (extend-protocol Message
+
   m/ChangeUsername
   (process-message [{:keys [username]} app]
-    (assoc app :username username)))
+    (assoc app :username username))
 
-(extend-protocol Message
   m/ChangePassword
   (process-message [{:keys [password]} app]
-    (assoc app :password password)))
+    (assoc app :password password))
 
-(extend-protocol Message
   m/DoLogin
   (process-message [_ app]
-    app))
+    app)
 
-(extend-protocol EventSource
-  m/DoLogin
-  (watch-channels [_ {:keys [username password] :as app}]
-    #{(rest/do-login username password)}))
-
-(extend-protocol Message
   m/LoginResult
   (process-message [{:keys [status body] :as response} app]
     (case status
       201 (do
             (utils/navigate! "/")
             (assoc app :user body :message nil :loggedin true :username nil :password nil))
-      (assoc app :username nil :password nil :user nil :message (:reason body) :loggedin false))))
+      (assoc app :username nil :password nil :user nil :message (:reason body) :loggedin false)))
 
-(extend-protocol Message
   m/DoLogout
   (process-message [_ app]
-    app))
+    app)
 
-(extend-protocol EventSource
-  m/DoLogout
-  (watch-channels [_ app]
-    #{(rest/do-logout)}))
-
-(extend-protocol Message
   m/LogoutResult
   (process-message [{:keys [status body] :as response} app]
     (case status
       200 (do
             (utils/navigate! "/login")
             (assoc app :username nil :password nil :user nil :message nil :loggedin false)))))
+
+
+(extend-protocol EventSource
+
+  m/DoLogin
+  (watch-channels [_ {:keys [username password] :as app}]
+    #{(rest/do-login username password)})
+
+    m/DoLogout
+  (watch-channels [_ app]
+    #{(rest/do-logout)}))
